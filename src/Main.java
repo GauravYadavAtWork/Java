c59d013f3d86724402701f47bdeb26112860373d
Gaurav Yadav <gauravyatwork@gmail.com>
2024-08-10 13:30:55 +0530

    made packages

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Student S1 = new Student("Gaurav Yadav", 21, "Male", "PSIT, Kanput", 72);
-        Human h2 = new Human("Gaurav Yadav", 21, "Male");
-        Human h3 = new Human();
-        S1.introduce();
-        Human.getPopulation();
-    }
-}
---
+++ src/package2/Main.java
@@ -0,0 +1,7 @@
+package package2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi package 2");
+    }
+}
