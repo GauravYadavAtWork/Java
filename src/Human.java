b45a389826001e7c50a3518431663dfb909e0c1c
Gaurav Yadav <gauravyatwork@gmail.com>
2024-08-10 11:06:25 +0530

    Java programs

--- src/Human.java
+++ src/Human.java
@@ -35,7 +35,7 @@ public class Human {
         Human h1 = new Human("Gaurav Yadav", 21, "Male");
         Human h2 = new Human("Gaurav Yadav", 21, "Male");
         Human h3 = new Human();
-        h1.introduce();
-        Human.getPopulation();
+        h1.introduce();          // object methods accessed with objects
+        Human.getPopulation();  // static methods accessed with class
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         Human h1 = new Human("Gaurav Yadav", 21, "Male");
         Human h2 = new Human("Gaurav Yadav", 21, "Male");
         Human h3 = new Human();
-        h1.introduce();
-        Human.getPopulation();
+        h1.introduce();          // object methods accessed with objects
+        Human.getPopulation();  // static methods accessed with class
     }
 }
